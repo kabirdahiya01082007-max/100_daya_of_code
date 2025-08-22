6ac065eb804b5b1d84816324da11ad6ab2fe5221
kabirdahiya01082007-max <kabirdahiya01082007@gmail.com>
2025-08-22 22:00:54 +0530

    _____

---
+++ q1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int n1, n2, sum;
+    printf("enter number 1 =>"); scanf("%d", &n1);
+    printf("enter number 2 =>"); scanf("%d", &n2);
+    sum = n1 + n2;
+    printf("The sum is %d", sum);
+    return 0;
+}
